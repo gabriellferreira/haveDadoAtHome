5c70eaefd01d1dd693714bff96dd2e59df716d8c
Gabriel Ferreira <gabriel.lucas@dextra-sw.com>
2018-05-17 18:52:43 -0300

    user name dialog

--- app/src/main/java/br/com/havedadoathome/presentation/view/activity/MainActivity.kt
+++ app/src/main/java/br/com/havedadoathome/presentation/view/activity/MainActivity.kt
@@ -1,24 +1,25 @@
 package br.com.havedadoathome.presentation.view.activity
 
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
-import br.com.havedadoathome.R
+import android.text.InputType
+import android.widget.EditText
 import br.com.havedadoathome.baseclean.presentation.view.MainContract
 import br.com.havedadoathome.presentation.internal.di.AppApplication
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 
 class MainActivity : BaseActivity<MainContract.Presenter, MainContract.View>(),
         MainContract.View {
+
+    private var userName: String? = null
 
     override fun createPresenter(): MainContract.Presenter {
         AppApplication.applicationComponent.inject(this)
@@ -33,6 +34,7 @@ class MainActivity : BaseActivity<MainContract.Presenter, MainContract.View>(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        askUserInfo()
         initListeners()
     }
 
@@ -41,13 +43,25 @@ class MainActivity : BaseActivity<MainContract.Presenter, MainContract.View>(),
             randomNumber()
         }
         val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("dado")
-        myRef.addValueEventListener(object : ValueEventListener {
+        val myRefDado = database.getReference("dado")
+        val myRefUser = database.getReference("user")
+        myRefDado.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 val value = dataSnapshot.getValue(Int::class.java)
                 numero_sorteado.text = value.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+        myRefUser.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue(String::class.java)
+                username.text = value.toString()
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -59,7 +73,7 @@ class MainActivity : BaseActivity<MainContract.Presenter, MainContract.View>(),
         val rand = Random()
         val value = rand.nextInt(MAX_DICE_NUMBER)
 
-        if (value == 0){
+        if (value == 0) {
             randomNumber()
         }
 
@@ -74,6 +88,7 @@ class MainActivity : BaseActivity<MainContract.Presenter, MainContract.View>(),
                 .setMessage("Seu numero sorteado foi: " + value)
                 .setPositiveButton("ok", { _, _ ->
                     writeNumberServer(value)
+                    writeNameServer(userName)
                     numero_sorteado.text = value.toString()
                 })
                 .setIcon(android.R.drawable.ic_dialog_alert)
@@ -85,5 +100,23 @@ class MainActivity : BaseActivity<MainContract.Presenter, MainContract.View>(),
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("dado")
         myRef.setValue(dadoNumber)
+    }
+
+    private fun writeNameServer(name: String? = "desconhecido") {
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("user")
+        myRef.setValue(name)
+    }
+
+    private fun askUserInfo() {
+        val alert = AlertDialog.Builder(this)
+        alert.setTitle("Qual é o seu nome?")
+        val input = EditText(this)
+        input.inputType = InputType.TYPE_CLASS_NUMBER
+        alert.setView(input)
+        alert.setPositiveButton("Xablau") { _, _ ->
+            userName = input.text.toString()
+        }
+        alert.show()
     }
 }
